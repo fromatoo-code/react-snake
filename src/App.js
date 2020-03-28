d533c9f8cf8e0f157dc38289bf329ee68e1c681c
askokr <asko.kriiska@gmail.com>
2020-03-28 17:14:06 +0200

    New logos & git corner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 
 import ReactSnake from './components/ReactSnake';
 
 function App() {
   return (
     <div className="App">
+      <GithubCorner href="https://github.com/askokr/react-snake" />
       <ReactSnake />
     </div>
   );
