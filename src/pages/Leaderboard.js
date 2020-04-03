6051d724b94612e2621b2bb5d82befb0d919e80b
askokr <asko.kriiska@gmail.com>
2020-04-03 15:22:57 +0300

    First implementation of local score keeping

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { LOCAL_SPEED, LOCAL_GRID, DEFAULT_SPEED, DEFAULT_GRID, LOOCAL_USERNAME } from '../utils/variables';
+import { LOCAL_SPEED, LOCAL_GRID, DEFAULT_SPEED, DEFAULT_GRID, LOOCAL_USERNAME, LOOCAL_BEST } from '../utils/variables';
 import { loadFromLocalStorage, addToLocalStorage } from '../utils/storageUtils';
 
 const Wrapper = styled.div`
@@ -28,23 +28,13 @@ const ResultName = styled.span`
   font-weight: bold;
 `;
 
-const mockData = [
-  { name: 'Asko', result: '10', speed: '3', },
-  { name: 'Kristina', result: '9', speed: '3', },
-  { name: 'Ergo', result: '8', speed: '3', },
-  { name: 'Triin', result: '7', speed: '3', },
-  { name: 'Asko2', result: '6', speed: '3', },
-  { name: 'Asko3', result: '5', speed: '3', },
-  { name: 'Asko4', result: '4', speed: '3', },
-  { name: 'Asko5', result: '3', speed: '3', },
-  { name: 'Asko6', result: '2', speed: '3', },
-  { name: 'Asko7', result: '2', speed: '2', },
-];
-
 export default () => {
   const speed = loadFromLocalStorage(LOCAL_SPEED, DEFAULT_SPEED);
   const grisSize = loadFromLocalStorage(LOCAL_GRID, DEFAULT_GRID);
   const [username, setUsername] = useState(loadFromLocalStorage(LOOCAL_USERNAME || ''));
+
+  const key = `${LOOCAL_BEST}${grisSize}`;
+  const results = loadFromLocalStorage(key || [])
 
   const updateUserName = (name) =>{
     addToLocalStorage(name, LOOCAL_USERNAME);
@@ -55,9 +45,9 @@ export default () => {
       <Input placeholder="username" value={username} onChange={(e) => updateUserName(e.target.value)}></Input>
       <Intro>{`${username ? `${username}, y`: 'Y'}ou are playing with on a size-${grisSize} grid at speed ${speed}`}</Intro>
       <Intro>The best results for this grid on this browser are:</Intro>
-      {mockData.map(data =>
-        <ResultData key={`${data.name}-${data.result}-${data.speed}`}>
-          <ResultName>{data.name}</ResultName>{` - ${data.result} at speed ${data.speed}`}
+      {results.map((data, i) =>
+        <ResultData key={`${data.name}-${data.score}-${data.speed}`}>
+          {i + 1}. <ResultName>{data.name}</ResultName>{` - ${data.score} at speed ${data.speed}`}
         </ResultData>)}
     </Wrapper>
   )
--- src/utils/gettersAndSetters.js
+++ src/utils/gettersAndSetters.js
@@ -1,9 +1,39 @@
 import snakeFireworks from '../images/snake-fireworks.jpg';
 
+import { addToLocalStorage, loadFromLocalStorage } from '../utils/storageUtils';
+import {
+  LOCAL_GRID,
+  DEFAULT_GRID,
+  LOCAL_SPEED,
+  DEFAULT_SPEED,
+  LOOCAL_USERNAME,
+  LOOCAL_BEST,
+ } from '../utils/variables';
+
 export const getBackgroundImage = () => {
   return snakeFireworks;
 };
 
 export const setHightScore = (score) => {
-  console.log(score);
+  const user = loadFromLocalStorage(LOOCAL_USERNAME);
+  if (user) {
+    const grid = loadFromLocalStorage(LOCAL_GRID, DEFAULT_GRID);
+    const speed = loadFromLocalStorage(LOCAL_SPEED, DEFAULT_SPEED);
+    const key = `${LOOCAL_BEST}${grid}`;
+    const relevantResults = loadFromLocalStorage(key, []);
+    const newResults = relevantResults;
+    const newResult = { name: user, score, speed };
+    for (let i = 0; i < relevantResults.length; i +=1) {
+      if (relevantResults[i].score < score || (relevantResults[i].score === score && relevantResults[i].speed < speed)) {
+        newResults.splice(i, 0, newResult);
+        break;
+      }
+    }
+
+    if (newResults.length === relevantResults.length) newResults.push(newResult);
+
+    const finalizedResults = newResults.slice(0, 10);
+
+    addToLocalStorage(finalizedResults, key);
+  };
 };
