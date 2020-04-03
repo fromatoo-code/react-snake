8600d7a94d173ae42afa1c5595928d380e77755d
askokr <asko.kriiska@gmail.com>
2020-04-03 09:00:13 +0300

    Delete unused utils

--- src/utils/storageUtils.js
+++ src/utils/storageUtils.js
@@ -6,57 +6,22 @@ export const addToLocalStorage = (value, storageKey) => {
     window.localStorage.setItem(storageKey, JSON.stringify(value));
   }
 };
-export const addToSessionStorage = (value, storageKey) => {
-  if (!isNil(value)) {
-    window.sessionStorage.setItem(storageKey, JSON.stringify(value));
-  }
-};
-export const addToStorage = (value, storageKey) => {
-  addToLocalStorage(value, storageKey);
-  addToSessionStorage(value, storageKey);
-};
 
 // DELETE
 export const deleteFromLocalStorage = (storageKey) => {
-    try {
-      window.localStorage.removeItem(storageKey);
-    } catch (error) {
-      console.warn('Could not remove item from local storage');
-    }
-  };
-  export const deleteFromSessionStorage = (storageKey) => {
-    try {
-      window.sessionStorage.removeItem(storageKey);
-    } catch (error) {
-      console.warn('Could not remove item from session storage');
-    }
-  };
-  export const deleteFromStorage = (storageKey) => {
-    deleteFromLocalStorage(storageKey);
-    deleteFromSessionStorage(storageKey);
-  };
+  try {
+    window.localStorage.removeItem(storageKey);
+  } catch (error) {
+    console.warn('Could not remove item from local storage');
+  }
+};
 
   // LOAD
-  export const loadFromSessionStorage = (storageKey) => {
-    try {
-      return JSON.parse(window.sessionStorage.getItem(storageKey));
-    } catch (error) {
-      console.warn('Could not get read session storage');
-    }
-    return null;
-  };
-  export const loadFromLocalStorage = (storageKey, defaultValue) => {
-    try {
-      return JSON.parse(window.localStorage.getItem(storageKey)) || defaultValue;
-    } catch (error) {
-      console.warn('Could not get read local storage');
-    }
-    return null;
+export const loadFromLocalStorage = (storageKey, defaultValue) => {
+  try {
+    return JSON.parse(window.localStorage.getItem(storageKey)) || defaultValue;
+  } catch (error) {
+    console.warn('Could not get read local storage');
   }
-  export const loadFromStorage = (storageKey, initialValue = null) => {
-    const sessionStorageItem = loadFromSessionStorage(storageKey);
-    if (!isNil(sessionStorageItem)) return sessionStorageItem;
-    const localStorageItem = loadFromLocalStorage(storageKey);
-    if (!isNil(localStorageItem)) return localStorageItem;
-    return initialValue;
-  }
+  return null;
+};
